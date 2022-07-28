4c248cc3b1f9ea8f3b535123d31e88c69c813601
Accalia <Accalia@Elementia.me>
2022-07-27 21:24:04 -0400

    fix kiosk mode for slideshows

--- bundles/slideshow/overlay.ts
+++ bundles/slideshow/overlay.ts
@@ -6,9 +6,13 @@ import { CyclicUpdater } from './updater'
 import { GetAlmanac } from './weather'
 
 const fadein: number = 30 * 60 * 100
-const maxOpacity: number = 0.85 // in percent.
+const maxOpacity: number = 0.75 // in percent.
+
+const kioskMode = new URLSearchParams(window.location.search).has('kiosk')
+const overlay = document.querySelector('.overlay') as HTMLElement
 
 const updateOverlay = () => {
+  if (!kioskMode) return
   const times = GetAlmanac()
   const now = Date.now()
   let offset = 0
@@ -17,11 +21,18 @@ const updateOverlay = () => {
   } else if (now > times.sunset) {
     offset = now - times.sunset
   }
-  const overlay = document.querySelector('.overlay') as HTMLElement
   if (overlay) {
     overlay.style.opacity = `${Math.min(maxOpacity * (offset / fadein), maxOpacity)}`
   }
   return Promise.resolve()
+}
+
+if (kioskMode && overlay) {
+  overlay.classList.remove('hide')
+
+  for (const elem of document.querySelectorAll('.pixel')) {
+    elem.classList.remove('hide')
+  }
 }
 
 export default CyclicUpdater.create(updateOverlay, 100)
