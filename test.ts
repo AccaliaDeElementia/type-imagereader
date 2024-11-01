f9f958af225a282a22d76027b43c2e277f61d05c
Accalia <Accalia@Elementia.me>
2024-10-31 20:57:34 -0400

    remove unneded test file

--- test.ts
+++
@@ -1,9 +0,0 @@
-import fsWalker from './utils/fswalker'
-
-fsWalker('/data', async (items: Array<{ path: string, isFile: boolean }>, _: number) => {
-  const dirs = items.filter(item => !item.isFile)
-  dirs.forEach(dir => {
-    console.log(dir.path)
-  })
-  await Promise.resolve()
-}).then(() => { console.log('done!') }, (err) => { console.error('whoops!', err) })
