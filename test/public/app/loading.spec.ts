9745706d92fdb5f8bb73c12e9b1782a609d4cec0
Accalia <Accalia@Elementia.me>
2025-06-17 16:25:27 -0400

    update Loading tests

--- test/public/app/loading.spec.ts
+++ test/public/app/loading.spec.ts
@@ -1,12 +1,9 @@
 'use sanity'
 
 import { expect } from 'chai'
-import { suite, test } from '@testdeck/mocha'
-import * as sinon from 'sinon'
-
+import Sinon from 'sinon'
 import { JSDOM } from 'jsdom'
 import { render } from 'pug'
-
 import { PubSub } from '../../../public/scripts/app/pubsub'
 import { Loading } from '../../../public/scripts/app/loading'
 import { Cast } from '../../testutils/TypeGuards'
@@ -18,90 +15,135 @@ html
     nav#navbar
     div#loadingScreen
 `
-
-@suite
-export class AppLoadingTests {
-  consoleError: sinon.SinonStub
-  existingWindow: Window & typeof globalThis
-  existingDocument: Document
-  dom: JSDOM
-  constructor() {
-    this.existingWindow = global.window
-    this.existingDocument = global.document
-    this.dom = new JSDOM('', {})
-    this.consoleError = sinon.stub()
-  }
-
-  before(): void {
-    this.dom = new JSDOM(render(markup), {
+describe('public/app/loading function Init()', () => {
+  const existingWindow: Window & typeof globalThis = global.window
+  const existingDocument: Document = global.document
+  let dom: JSDOM = new JSDOM('', {})
+  beforeEach(() => {
+    dom = new JSDOM(render(markup), {
       url: 'http://127.0.0.1:2999',
     })
-    this.existingWindow = global.window
-    global.window = Cast<Window & typeof globalThis>(this.dom.window)
-    this.existingDocument = global.document
-    global.document = this.dom.window.document
-    this.consoleError = sinon.stub(global.window.console, 'error')
+    global.window = Cast<Window & typeof globalThis>(dom.window)
+    global.document = dom.window.document
     PubSub.subscribers = {}
     PubSub.deferred = []
+    Loading.overlay = null
+    Loading.navbar = null
     Loading.Init()
-  }
-
-  after(): void {
-    global.window = this.existingWindow
-    global.document = this.existingDocument
-  }
-
-  @test
-  'Loading subscribes to Loading:Error'(): void {
+  })
+  afterEach(() => {
+    global.window = existingWindow
+    global.document = existingDocument
+  })
+  after(() => {
+    Sinon.restore()
+  })
+  it('should subscribe to Loading:Error', () => {
     expect(PubSub.subscribers['LOADING:ERROR']).to.have.length(1)
-  }
-
-  @test
-  'Loading subscribes to Loading:Success'(): void {
+  })
+  it('should subscribe to Loading:Success', () => {
     expect(PubSub.subscribers['LOADING:SUCCESS']).to.have.length(1)
-  }
-
-  @test
-  'Loading subscribes to Loading:Hide'(): void {
+  })
+  it('should subscribe to Loading:Hide', () => {
     expect(PubSub.subscribers['LOADING:HIDE']).to.have.length(1)
-  }
-
-  @test
-  'Loading subscribes to Loading:Show'(): void {
+  })
+  it('should subscribe to Loading:Show', () => {
     expect(PubSub.subscribers['LOADING:SHOW']).to.have.length(1)
-  }
-
-  @test
-  'Publishing Loading:Show shows the loading overlay'(): void {
-    const overlay = this.dom.window.document.querySelector<HTMLElement>('#loadingScreen')
+  })
+  it('should select overlay for disabling input', () => {
+    expect(Loading.overlay).to.not.equal(null)
+  })
+  it('should select navbar for feedback', () => {
+    expect(Loading.navbar).to.not.equal(null)
+  })
+})
+describe('public/app/loading subscriber "Loading:Show" and "Loading:Hide"', () => {
+  const existingWindow: Window & typeof globalThis = global.window
+  const existingDocument: Document = global.document
+  let dom: JSDOM = new JSDOM('', {})
+  beforeEach(() => {
+    dom = new JSDOM(render(markup), {
+      url: 'http://127.0.0.1:2999',
+    })
+    global.window = Cast<Window & typeof globalThis>(dom.window)
+    global.document = dom.window.document
+    PubSub.subscribers = {}
+    PubSub.deferred = []
+    Loading.overlay = null
+    Loading.navbar = null
+    Loading.Init()
+  })
+  afterEach(() => {
+    global.window = existingWindow
+    global.document = existingDocument
+  })
+  after(() => {
+    Sinon.restore()
+  })
+  it('should show the loading overlay for "Loading:Show"', () => {
+    const overlay = dom.window.document.querySelector<HTMLElement>('#loadingScreen')
     overlay?.style.setProperty('display', 'none')
     PubSub.Publish('Loading:Show')
     expect(overlay?.style.getPropertyValue('display')).to.equal('block')
-  }
-
-  @test
-  'Publishing Loading:Hide removes the loading overlay'(): void {
-    const overlay = this.dom.window.document.querySelector<HTMLElement>('#loadingScreen')
+  })
+  it('should hide the loading overlay for "Loading:Hide"', () => {
+    const overlay = dom.window.document.querySelector<HTMLElement>('#loadingScreen')
     overlay?.style.setProperty('display', 'block')
     PubSub.Publish('Loading:Hide')
     expect(overlay?.style.getPropertyValue('display')).to.equal('none')
-  }
-
-  @test
-  'Publishing Loading:Error with message logs message to web console'(): void {
+  })
+  it('should tolerate missing overlay for "Loading:Show"', () => {
+    Loading.overlay = null
+    expect(() => {
+      PubSub.Publish('Loading:Show')
+    }).to.not.throw()
+  })
+  it('should hide the loading overlay for "Loading:Hide"', () => {
+    Loading.overlay = null
+    expect(() => {
+      PubSub.Publish('Loading:Hide')
+    }).to.not.throw()
+  })
+})
+describe('public/app/loading subscriber "Loading:Error"', () => {
+  let consoleError = Sinon.stub()
+  const existingWindow: Window & typeof globalThis = global.window
+  const existingDocument: Document = global.document
+  let dom: JSDOM = new JSDOM('', {})
+  beforeEach(() => {
+    dom = new JSDOM(render(markup), {
+      url: 'http://127.0.0.1:2999',
+    })
+    global.window = Cast<Window & typeof globalThis>(dom.window)
+    global.document = dom.window.document
+    consoleError = Sinon.stub(global.window.console, 'error')
+    PubSub.subscribers = {}
+    PubSub.deferred = []
+    Loading.overlay = null
+    Loading.navbar = null
+    Loading.Init()
+  })
+  afterEach(() => {
+    global.window = existingWindow
+    global.document = existingDocument
+  })
+  after(() => {
+    Sinon.restore()
+  })
+  it('should log message to web console', () => {
     const message = `error message! ${Math.random()}`
     PubSub.Publish('Loading:Error', message)
-    expect(this.consoleError.calledOnceWithExactly(message)).to.equal(true)
-  }
-
-  @test
-  'Publishing Loading:Error without message logs no message to web console'(): void {
-    PubSub.Publish('Loading:Error')
-    expect(this.consoleError.called).to.equal(false)
-  }
-
-  @test
-  async 'Publishing Loading:Error hides loading overlay'(): Promise<void> {
+    expect(consoleError.calledOnceWithExactly(message)).to.equal(true)
+  })
+  it('should not log null message to web console', () => {
+    PubSub.Publish('Loading:Error', null)
+    expect(consoleError.called).to.equal(false)
+  })
+  it('should not log undefined message to web console', () => {
+    PubSub.Publish('Loading:Error', undefined)
+    expect(consoleError.called).to.equal(false)
+  })
+  it('should hide loading overlay', async () => {
     let hidden = false
     PubSub.Subscribe('Loading:Hide', async () => {
       hidden = true
@@ -113,11 +155,9 @@ export class AppLoadingTests {
       await sub(undefined, 'LOADING:ERROR') // execute all subscribers manually
     }
     expect(hidden).to.equal(true)
-  }
-
-  @test
-  async 'Publishing Loading:Error removes css transition style on navbar'(): Promise<void> {
-    const navbar = this.dom.window.document.querySelector<HTMLElement>('#navbar')
+  })
+  it('should remove css transition style on navbar', async () => {
+    const navbar = dom.window.document.querySelector<HTMLElement>('#navbar')
     navbar?.style.setProperty('transition', 'background-color 2s ease-in-out')
     const subs = PubSub.subscribers['LOADING:ERROR']
     assert(subs !== undefined)
@@ -125,11 +165,9 @@ export class AppLoadingTests {
       await sub(undefined, 'LOADING:ERROR') // execute all subscribers manually
     }
     expect(navbar?.style.getPropertyValue('transition')).to.equal('')
-  }
-
-  @test
-  async 'Publishing Loading:Error sets scary red background navbar'(): Promise<void> {
-    const navbar = this.dom.window.document.querySelector<HTMLElement>('#navbar')
+  })
+  it('should set scary red background navbar', async () => {
+    const navbar = dom.window.document.querySelector<HTMLElement>('#navbar')
     navbar?.style.removeProperty('background-color')
     const subs = PubSub.subscribers['LOADING:ERROR']
     assert(subs !== undefined)
@@ -137,10 +175,8 @@ export class AppLoadingTests {
       await sub(undefined, 'LOADING:ERROR') // execute all subscribers manually
     }
     expect(navbar?.style.getPropertyValue('background-color')).to.equal('rgb(255, 0, 0)')
-  }
-
-  @test
-  async 'Publishing Loading:Error sets a deferred function'(): Promise<void> {
+  })
+  it('should set a deferred function', async () => {
     expect(PubSub.deferred).to.have.length(0)
     const subs = PubSub.subscribers['LOADING:ERROR']
     assert(subs !== undefined)
@@ -148,11 +184,9 @@ export class AppLoadingTests {
       await sub(undefined, 'LOADING:ERROR') // execute all subscribers manually
     }
     expect(PubSub.deferred).to.have.length(1)
-  }
-
-  @test
-  async 'Publishing Loading:Error defers transition definition'(): Promise<void> {
-    const navbar = this.dom.window.document.querySelector<HTMLElement>('#navbar')
+  })
+  it('should defer transition definition', async () => {
+    const navbar = dom.window.document.querySelector<HTMLElement>('#navbar')
     navbar?.style.removeProperty('transition')
     const subs = PubSub.subscribers['LOADING:ERROR']
     assert(subs !== undefined)
@@ -163,11 +197,9 @@ export class AppLoadingTests {
       fn.method()
     })
     expect(navbar?.style.getPropertyValue('transition')).to.equal('background-color 2s ease-in-out')
-  }
-
-  @test
-  async 'Publishing Loading:Error deferres background-color change'(): Promise<void> {
-    const navbar = this.dom.window.document.querySelector<HTMLElement>('#navbar')
+  })
+  it('should defer background-color change', async () => {
+    const navbar = dom.window.document.querySelector<HTMLElement>('#navbar')
     navbar?.style.setProperty('background-color', '#FFFFFF')
     const subs = PubSub.subscribers['LOADING:ERROR']
     assert(subs !== undefined)
@@ -178,11 +210,33 @@ export class AppLoadingTests {
       fn.method()
     })
     expect(navbar?.style.getPropertyValue('background-color')).to.equal('')
-  }
-
-  @test
-  async 'Publishing Loading:Success removes css transition style on navbar'(): Promise<void> {
-    const navbar = this.dom.window.document.querySelector<HTMLElement>('#navbar')
+  })
+})
+describe('public/app/loading subscriber "Loading:Success"', () => {
+  const existingWindow: Window & typeof globalThis = global.window
+  const existingDocument: Document = global.document
+  let dom: JSDOM = new JSDOM('', {})
+  beforeEach(() => {
+    dom = new JSDOM(render(markup), {
+      url: 'http://127.0.0.1:2999',
+    })
+    global.window = Cast<Window & typeof globalThis>(dom.window)
+    global.document = dom.window.document
+    PubSub.subscribers = {}
+    PubSub.deferred = []
+    Loading.overlay = null
+    Loading.navbar = null
+    Loading.Init()
+  })
+  afterEach(() => {
+    global.window = existingWindow
+    global.document = existingDocument
+  })
+  after(() => {
+    Sinon.restore()
+  })
+  it('should remove css transition style on navbar', async () => {
+    const navbar = dom.window.document.querySelector<HTMLElement>('#navbar')
     navbar?.style.setProperty('transition', 'background-color 2s ease-in-out')
     const subs = PubSub.subscribers['LOADING:SUCCESS']
     assert(subs !== undefined)
@@ -190,11 +244,9 @@ export class AppLoadingTests {
       await sub(undefined, 'LOADING:SUCCESS') // execute all subscribers manually
     }
     expect(navbar?.style.getPropertyValue('transition')).to.equal('')
-  }
-
-  @test
-  async 'Publishing Loading:Success sets soothing green background navbar'(): Promise<void> {
-    const navbar = this.dom.window.document.querySelector<HTMLElement>('#navbar')
+  })
+  it('should set soothing green background navbar', async () => {
+    const navbar = dom.window.document.querySelector<HTMLElement>('#navbar')
     navbar?.style.removeProperty('background-color')
     const subs = PubSub.subscribers['LOADING:SUCCESS']
     assert(subs !== undefined)
@@ -202,10 +254,8 @@ export class AppLoadingTests {
       await sub(undefined, 'LOADING:SUCCESS') // execute all subscribers manually
     }
     expect(navbar?.style.getPropertyValue('background-color')).to.equal('rgb(0, 170, 0)')
-  }
-
-  @test
-  async 'Publishing Loading:Success sets a deferred function'(): Promise<void> {
+  })
+  it('should set a deferred function', async () => {
     expect(PubSub.deferred).to.have.length(0)
     const subs = PubSub.subscribers['LOADING:SUCCESS']
     assert(subs !== undefined)
@@ -213,11 +263,9 @@ export class AppLoadingTests {
       await sub(undefined, 'LOADING:SUCCESS') // execute all subscribers manually
     }
     expect(PubSub.deferred).to.have.length(1)
-  }
-
-  @test
-  async 'Publishing Loading:Success defers transition definition'(): Promise<void> {
-    const navbar = this.dom.window.document.querySelector<HTMLElement>('#navbar')
+  })
+  it('should defer transition definition', async () => {
+    const navbar = dom.window.document.querySelector<HTMLElement>('#navbar')
     navbar?.style.removeProperty('transition')
     const subs = PubSub.subscribers['LOADING:SUCCESS']
     assert(subs !== undefined)
@@ -228,11 +276,9 @@ export class AppLoadingTests {
       fn.method()
     })
     expect(navbar?.style.getPropertyValue('transition')).to.equal('background-color 2s ease-in-out')
-  }
-
-  @test
-  async 'Publishing Loading:Success deferres background-color change'(): Promise<void> {
-    const navbar = this.dom.window.document.querySelector<HTMLElement>('#navbar')
+  })
+  it('should defer background-color change', async () => {
+    const navbar = dom.window.document.querySelector<HTMLElement>('#navbar')
     navbar?.style.setProperty('background-color', '#FFFFFF')
     const subs = PubSub.subscribers['LOADING:SUCCESS']
     assert(subs !== undefined)
@@ -243,21 +289,61 @@ export class AppLoadingTests {
       fn.method()
     })
     expect(navbar?.style.getPropertyValue('background-color')).to.equal('')
-  }
-
-  @test
-  'IsLoading is true when overlay is displayed as block element'(): void {
-    const overlay = this.dom.window.document.querySelector<HTMLElement>('#loadingScreen')
-    overlay?.style.setProperty('display', 'block')
-    expect(Loading.IsLoading()).to.equal(true)
-  }
-
-  @test
-  'IsLoading is false when overlay is not displayed as block element'(): void {
-    const overlay = this.dom.window.document.querySelector<HTMLElement>('#loadingScreen')
-    for (const display of ['flex', 'none', 'inline-block']) {
-      overlay?.style.setProperty('display', display)
-      expect(Loading.IsLoading()).to.equal(false)
-    }
-  }
-}
+  })
+})
+describe('public/app/loading subscriber "Loading:Success"', () => {
+  const existingWindow: Window & typeof globalThis = global.window
+  const existingDocument: Document = global.document
+  let dom: JSDOM = new JSDOM('', {})
+  beforeEach(() => {
+    dom = new JSDOM(render(markup), {
+      url: 'http://127.0.0.1:2999',
+    })
+    global.window = Cast<Window & typeof globalThis>(dom.window)
+    global.document = dom.window.document
+    PubSub.subscribers = {}
+    PubSub.deferred = []
+    Loading.overlay = null
+    Loading.navbar = null
+    Loading.Init()
+  })
+  afterEach(() => {
+    global.window = existingWindow
+    global.document = existingDocument
+  })
+  after(() => {
+    Sinon.restore()
+  })
+  const testCases: Array<[string, boolean]> = [
+    ['inline', false],
+    ['block', true],
+    ['contents', false],
+    ['flex', false],
+    ['grid', false],
+    ['inline-block', false],
+    ['inline-flex', false],
+    ['inline-grid', false],
+    ['inline-table', false],
+    ['list-item', false],
+    ['run-in', false],
+    ['table', false],
+    ['table-caption', false],
+    ['table-column-group', false],
+    ['table-header-group', false],
+    ['table-footer-group', false],
+    ['table-row-group', false],
+    ['table-cell', false],
+    ['table-column', false],
+    ['table-row', false],
+    ['none', false],
+    ['initial', false],
+    ['inherit', false],
+  ]
+  testCases.forEach(([style, expected]) => {
+    it(`should ${expected ? '' : 'not '}consider ${style} as IsLoading`, () => {
+      const overlay = dom.window.document.querySelector<HTMLElement>('#loadingScreen')
+      overlay?.style.setProperty('display', style)
+      expect(Loading.IsLoading()).to.equal(expected)
+    })
+  })
+})
