c84e31dfefd163feabe9b6851abc87a954476786
Accalia <Accalia@Elementia.me>
2026-05-10 15:27:43 -0400

    updated isPicture contract tests to use real HTMLElement objects instead of ducktyped fakes

--- test/contracts/listing/isPicture.spec.ts
+++ test/contracts/listing/isPicture.spec.ts
@@ -1,366 +1,335 @@
 'use sanity'
 
 import { expect } from 'chai'
+import { JSDOM } from 'jsdom'
 import { isPicture } from '#contracts/listing.js'
+import { mountDom, unmountDom } from '#testutils/dom.js'
+
+const dom = new JSDOM('<html></html>')
 
 describe('Contracts: isPicture()', () => {
-  const cases: Array<[string, unknown, boolean]> = [
-    ['null', null, false],
-    ['undefined', undefined, false],
-    ['empty array', [], false],
-    ['array', [{}], false],
-    ['number', 4.71, false],
-    ['string', 'foo!', false],
-    ['empty object', {}, false],
+  beforeEach(() => {
+    mountDom(dom)
+  })
+  afterEach(() => {
+    unmountDom()
+  })
+
+  const cases: Array<[string, () => unknown, boolean]> = [
+    ['null', () => null, false],
+    ['undefined', () => undefined, false],
+    ['empty array', () => [], false],
+    ['array', () => [{}], false],
+    ['number', () => 4.71, false],
+    ['string', () => 'foo!', false],
+    ['empty object', () => ({}), false],
     [
       'null path',
-      {
+      () => ({
         path: null,
         name: '',
         seen: false,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'undefined path',
-      {
+      () => ({
         path: undefined,
         name: '',
         seen: false,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'missing path',
-      {
+      () => ({
         name: '',
         seen: false,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'invalid path',
-      {
+      () => ({
         path: {},
         name: '',
         seen: false,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'null name',
-      {
+      () => ({
         path: 'foo!',
         name: null,
         seen: false,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'undefined name',
-      {
+      () => ({
         path: 'foo!',
         name: undefined,
         seen: false,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'missing name',
-      {
+      () => ({
         path: 'foo!',
         seen: false,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'invalid name',
-      {
+      () => ({
         path: 'foo!',
         name: 534,
         seen: false,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'null seen',
-      {
+      () => ({
         path: 'foo!',
         name: 'bar!',
         seen: null,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'undefined seen',
-      {
+      () => ({
         path: 'foo!',
         name: 'bar!',
         seen: undefined,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'missing seen',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'invalid seen',
-      {
+      () => ({
         path: 'foo!',
         name: 'bar!',
         seen: 'false',
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'missing index',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       true,
     ],
     [
       'null index',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: null,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'undefined index',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: undefined,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       true,
     ],
     [
       'invalid index',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: [],
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'missing page',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       true,
     ],
     [
       'null page',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: 0,
         page: null,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'undefined page',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: 0,
         page: undefined,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       true,
     ],
     [
       'invalid page',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: 0,
         page: 'one',
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       false,
     ],
     [
       'missing element',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: 0,
         page: 0,
-      },
+      }),
       true,
     ],
     [
       'null element',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: 0,
         page: 0,
         element: null,
-      },
+      }),
       false,
     ],
     [
       'undefined element',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: 0,
         page: 0,
         element: undefined,
-      },
+      }),
       true,
     ],
     [
       'invalid element',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: 0,
         page: 0,
         element: {},
-      },
+      }),
       false,
     ],
     [
       'valid API Picture',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
-      },
+      }),
       true,
     ],
     [
       'valid web Picture',
-      {
+      () => ({
         path: 'foo!',
         name: '',
         seen: false,
         index: 0,
         page: 0,
-        element: {
-          style: '', // a "fake" HTMLElement
-        },
-      },
+        element: dom.window.document.createElement('div'),
+      }),
       true,
     ],
   ]
-  cases.forEach(([title, obj, expected]) => {
+  cases.forEach(([title, build, expected]) => {
     it(`should return ${expected} for ${title}`, () => {
-      expect(isPicture(obj)).to.equal(expected)
+      expect(isPicture(build())).to.equal(expected)
     })
   })
 })
