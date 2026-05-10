805c9c743bff25591fe2e693b5ca34c870335469
Accalia <Accalia@Elementia.me>
2026-05-10 15:19:41 -0400

    updated markup contract tests to use real HTMLElement objects instead of ducktyped fakes

--- test/contracts/markup.spec.ts
+++ test/contracts/markup.spec.ts
@@ -1,19 +1,30 @@
 'use sanity'
 
 import { expect } from 'chai'
+import { JSDOM } from 'jsdom'
 import { isHTMLElement } from '#contracts/markup.js'
+import { mountDom, unmountDom } from '#testutils/dom.js'
+
+const dom = new JSDOM('<html></html>')
 
 describe('Contracts: isHTMLElement()', () => {
-  const cases: Array<[string, unknown, boolean]> = [
-    ['null', null, false],
-    ['undefined', undefined, false],
-    ['dict', {}, false],
-    ['array', [], false],
-    ['"HTMLElement"', { style: {} }, true],
+  beforeEach(() => {
+    mountDom(dom)
+  })
+  afterEach(() => {
+    unmountDom()
+  })
+
+  const cases: Array<[string, () => unknown, boolean]> = [
+    ['null', () => null, false],
+    ['undefined', () => undefined, false],
+    ['dict', () => ({}), false],
+    ['array', () => [], false],
+    ['real HTMLElement', () => dom.window.document.createElement('div'), true],
   ]
-  cases.forEach(([title, obj, expected]) => {
+  cases.forEach(([title, build, expected]) => {
     it(`should return ${expected} for ${title}`, () => {
-      expect(isHTMLElement(obj)).to.equal(expected)
+      expect(isHTMLElement(build())).to.equal(expected)
     })
   })
 })
